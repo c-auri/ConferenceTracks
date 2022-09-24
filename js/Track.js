33fc7d2f9e9c0d7c90bb027004dab05fd38443da
Christoph Aurich <aurichchristoph@gmail.com>
2022-09-24 19:36:51 +0200

    Add Track class

---
+++ js/Track.js
@@ -0,0 +1,50 @@
+const Session = require("./Session")
+
+class Track {
+    constructor(name, settings) {
+        this.name = name
+        
+        this.morning = new Session(
+            settings.year, 
+            settings.monthIndex, 
+            settings.day,
+            settings.morningBeginningHour,
+            settings.morningEarliestEndHour,
+            settings.morningLatestEndHour)
+        
+        this.afternoon = new Session(
+            settings.year, 
+            settings.monthIndex, 
+            settings.day,
+            settings.afternoonBeginningHour,
+            settings.afternoonEarliestEndHour,
+            settings.afternoonLatestEndHour)
+    }
+
+    get isSatisfied() {
+        return this.morning.isSatisfied 
+            && this.afternoon.isSatisfied
+    }
+
+    get isMaxedOut() {
+        return this.morning.isMaxedOut 
+            && this.afternoon.isMaxedOut
+    }
+
+    get talks() {
+        return this.morning.talks.concat(this.afternoon.talks)
+    }
+
+    tryAdd(talk) {
+        let added = this.morning.tryAdd(talk)
+
+        if (!added) {
+            added = this.afternoon.tryAdd(talk)
+        }
+
+        return added
+    }
+}
+
+
+module.exports = Track
