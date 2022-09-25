b71f68a56d5772bd4bc2fcfd8bbf9ab55a16ccaa
Christoph Aurich <aurichchristoph@gmail.com>
2022-09-25 20:15:31 +0200

    Parse input from file

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
-const { Talk, LightningTalk } = require('./js/TrackManagement/Talk')
-const solve = require('./js/solve')
+const { readFileSync } = require('fs');
+const { exit } = require('process');
 
-function main() {
-    const talks = [
-        new Talk('Writing Fast Tests Against Enterprise Rails', 60),
-        new Talk('Overdoing it in Python', 45),
-        new Talk('Lua for the Masses', 30),
-        new Talk('Ruby Errors from Mismatched Gem Versions', 45),
-        new Talk('Common Ruby Errors', 45),
-        new LightningTalk('Rails for Python Developers'),
-        new Talk('Communicating Over Distance', 60),
-        new Talk('Accounting-Driven Development', 45),
-        new Talk('Woah', 30),
-        new Talk('Sit Down and Write', 30),
-        new Talk('Pair Programming vs Noise', 45),
-        new Talk('Rails Magic', 60),
-        new Talk('Ruby on Rails: Why We Should Move On', 60),
-        new Talk('Clojure Ate Scala (on my project)', 45),
-        new Talk('Programming in the Boondocks of Seattle', 30),
-        new Talk('Ruby vs. Clojure for Back-End Development', 30),
-        new Talk('Ruby on Rails Legacy App Maintenance', 60),
-        new Talk('A World Without HackerNews', 30),
-        new Talk('User Interface CSS in Rails Apps', 30),
-    ]
+const Parse = require('./js/Parse');
+const solve = require('./js/solve');
 
+function main(filename) {
+    let talks = parseFile(filename)
     let solution = solve(talks)
 
     for (const track of solution.tracks) {
@@ -36,6 +18,20 @@ function main() {
     }
 }
 
+function parseFile(filename) {
+    let file
+
+    try {
+        file = readFileSync(filename, 'utf-8');
+    } catch (e) {
+        console.log('Bad filename: ' + filename)
+        exit()
+    }
+
+    return file.split(/\r?\n/).map(Parse.lineToTalk)
+}
+
 if (require.main === module) {
-    main();
+    let filename = process.argv[2] ? './input/' + process.argv[2] : './input/assignment.txt'
+    main(filename);
 }
