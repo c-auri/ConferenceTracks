7588bb9c4a0179113d1be7717c2b3936da14b554
Christoph Aurich <aurichchristoph@gmail.com>
2022-09-26 22:41:47 +0200

    Clean up Parser

--- js/Parser.js
+++ js/Parser.js
@@ -6,20 +6,6 @@ class Parser {
         return 'lightning'
     }
 
-    static #specifiesDuration(line) {
-        const lastToken = line.split(' ').slice(-1)
-        return /\d/.test(lastToken);
-    }
-
-    static #isLightningTalk(line) {
-        return line.slice(-1 * Parser.lightningIdentifier.length) == Parser.lightningIdentifier
-    }
-
-    static #splitTitleAndDuration(line) {
-        const tokens = line.split(' ')
-        return { title: tokens.slice(0, -1).join(' ').trim(), duration: parseInt(tokens.slice(-1)) }
-    }
-
     constructor(trackSettings) {
         this.trackSettings = trackSettings
     }
@@ -27,21 +13,47 @@ class Parser {
     lineToTalk(line) {
         line = line.trim()
 
-        if (!Parser.#specifiesDuration(line) && !Parser.#isLightningTalk(line)) {
+        if (!this.#specifiesDuration(line) && !this.#specifiesLightningTalk(line)) {
             throw new Error('No duration specified in: ' + line)
         }
 
-        if (Parser.#isLightningTalk(line)) {
-            const title = line.slice(0, line.length - Parser.lightningIdentifier.length).trim()
-            return new LightningTalk(title)
+        if (this.#specifiesLightningTalk(line)) {
+            return this.#parseLightningTalk(line)
         } else {
-            const { title, duration } = Parser.#splitTitleAndDuration(line)
+            return this.#parseTalk(line)
+        }
+    }
 
-            if (!this.trackSettings.canFit(duration)) {
-                throw new Error('Talk duration is too long.')
-            } else {
-                return new Talk(title, duration)
-            }
+    #specifiesDuration(line) {
+        const lastToken = line.split(' ').slice(-1)
+        return /\d/.test(lastToken);
+    }
+
+    #specifiesLightningTalk(line) {
+        return line.slice(-1 * Parser.lightningIdentifier.length) == Parser.lightningIdentifier
+    }
+
+    #parseLightningTalk(line) {
+        const title = line.slice(0, line.length - Parser.lightningIdentifier.length).trim()
+        return new LightningTalk(title)
+    }
+
+    #parseTalk(line) {
+        const { title, duration } = this.#extractTitleAndDuration(line)
+
+        if (!this.trackSettings.canFit(duration)) {
+            throw new Error('Talk duration is too long.')
+        } else {
+            return new Talk(title, duration)
+        }
+    }
+
+    #extractTitleAndDuration(line) {
+        const tokens = line.split(' ')
+
+        return { 
+            title: tokens.slice(0, -1).join(' ').trim(), 
+            duration: parseInt(tokens.slice(-1)) 
         }
     }
 }
